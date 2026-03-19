fe779364d5438bbfe63fd74a17bc7accc6836f65
dianaderevjanko <dianaderevjanko@gmail.com>
2026-03-19 13:53:02 +0200

    Pievienota konvertācija ar izveli

--- converter.py
+++ converter.py
@@ -3,7 +3,19 @@ KM_TO_MILES = 0.621371
 KG_TO_LBS = 2.20462
 L_TO_GALLONS = 0.264172
 USD_TO_EUR = 0.85235
-print("Konversijas kalkulators")
-izvele = input("Izvelies konvērsiju: 1)km<->mi, 2)kg<->lb 3)L<->gal 4)USD<->EUR"  )
+print('Konversijas kalkulators')
+izvele = input("Izvelies konvērsiju: 1)km<->mi, 2)kg<->lb 3)L<->gal 4)USD<->EUR   ")
 #pārbaudam lai izvēle būtu pareiza
-if izvele in {"1", "2", "3", "4"}: # vai rakstām if izvele == "1" or izvele == "2" or izvele == "3" or izvele == "4":
+if izvele == "1":
+    print('Izvelies virzienu: 1) KM/MI 2 )MI/KM ' )
+    virziens = input("Ievadi virzienu: ")
+    if virziens == "1":
+        vertiba = float(input("Ievadi vērtību, kuru vēlies konvertēt: "))
+        rezultats = vertiba * KM_TO_MILES
+        print(f"Rezultāts: {vertiba :.2f} km ir {rezultats:.2f} mi.")  #:.2f - noapaļo rezultātu līdz 2 cipariem aiz komata
+    elif virziens == "2":
+        vertiba = float(input("Ievadi vērtību, kuru vēlies konvertēt: "))
+        rezultats = vertiba / KM_TO_MILES
+        print(f"Rezultāts: {vertiba :.2f} mi ir {rezultats:.2f} km.")
+    else:
+        print("Kļūda! Lūdzu, izvēlies pareizo virzienu.")

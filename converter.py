290edff03d78548d69fc7ecf3af4c2d0e234727b
dianaderevjanko <dianaderevjanko@gmail.com>
2026-03-19 20:25:29 +0200

    Labota lietotāja ievade: pievienota komatu aizstāšana ar punktu

--- converter.py
+++ converter.py
@@ -19,3 +19,31 @@ if izvele == "1":
         print(f"Rezultāts: {vertiba :.2f} mi ir {rezultats:.2f} km.")
     else:
         print("Kļūda! Lūdzu, izvēlies pareizo virzienu.")
+if izvele == "2":
+    print('Izvelies virzienu: 1) KG/LB 2 )LB/KG ' )
+    virziens = input("Ievadi virzienu: ")
+    if virziens == "1":
+        vertiba = float(input("Ievadi vērtību, kuru vēlies konvertēt: "))
+        rezultats = vertiba * KG_TO_LBS
+        print(f"Rezultāts: {vertiba :.2f} kg ir {rezultats:.2f} lb.")  #:.2f - noapaļo rezultātu līdz 2 cipariem aiz komata
+    elif virziens == "2":
+        vertiba = float(input("Ievadi vērtību, kuru vēlies konvertēt: "))
+        rezultats = vertiba / KG_TO_LBS
+        print(f"Rezultāts: {vertiba :.2f} lb ir {rezultats:.2f} kg.")
+    else:
+        print("Kļūda! Lūdzu, izvēlies pareizo virzienu.")
+if izvele == "3":
+    print('Izvelies virzienu: 1) L/GAL 2 )GAL/L ' )
+    virziens = input("Ievadi virzienu: ")
+    if virziens == "1" or virziens == "2":
+        # Pajautājam vērtību un uzreiz salabojam iespējamo komata kļūdu
+        ievade = input("Ievadi vērtību, kuru vēlies konvertēt: ")
+        vertiba = float(ievade.replace(',', '.'))
+    if virziens == "1":
+        rezultats = vertiba * L_TO_GALLONS
+        print(f"Rezultāts: {vertiba :.2f} L ir {rezultats:.2f} gal.")  #:.2f - noapaļo rezultātu līdz 2 cipariem aiz komata
+    elif virziens == "2":
+        rezultats = vertiba / L_TO_GALLONS
+        print(f"Rezultāts: {vertiba :.2f} gal ir {rezultats:.2f} L.")
+    else:
+        print("Kļūda! Lūdzu, izvēlies pareizo virzienu.")

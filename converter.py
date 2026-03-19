78a9d3357a36a334b4a052c35011af70d709f9ec
dianaderevjanko <dianaderevjanko@gmail.com>
2026-03-19 12:54:06 +0200

    Pievienoti konstantes

---
+++ converter.py
@@ -0,0 +1,9 @@
+#konstances
+KM_TO_MILES = 0.621371
+KG_TO_LBS = 2.20462
+L_TO_GALLONS = 0.264172
+USD_TO_EUR = 0.85235
+print("Konversijas kalkulators")
+izvele = input("Izvelies konvērsiju: 1)km<->mi, 2)kg<->lb 3)L<->gal 4)USD<->EUR"  )
+#pārbaudam lai izvēle būtu pareiza
+if izvele in {"1", "2", "3", "4"}: # vai rakstām if izvele == "1" or izvele == "2" or izvele == "3" or izvele == "4":

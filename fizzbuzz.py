049661cb1aba0658029591d0ce23ae86abc4ec9d
dianaderevjanko <dianaderevjanko@gmail.com>
2026-03-20 16:35:17 +0200

    Pievienots for cikls un Jazz variācija (dalāmība ar 7)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -11,16 +11,17 @@ else:
     n = int(input("N netika norādīts. Ievadi ciklu skaitu N: "))
 
 print(f"\n--- Python FIZZBUZZ līdz {n} ---")
- # Pirmo pārbaudām kopīgo dalāmību (ar 3 UN 5)
+
+for skaitlis in range(1, n + 1):  # uzsākst cikls no 1 līdz N (ieskaitot)
+    # Pirmo pārbaudām kopīgo dalāmību (ar 3 UN 5)
     # Izmantojam modulo operatoru %, kas atgriež atlikumu (0 nozīmē, ka dalās)
-if skaitlis % 3 == 0 and skaitlis % 5 == 0: #skaitlis dalās ar 3 un 5
+    if skaitlis % 3 == 0 and skaitlis % 5 == 0:  # skaitlis dalās ar 3 un 5
         print("FizzBuzz")
-        
-    elif skaitlis % 3 == 0:  #Dalot skaitli ar 3, atlikums ir nulle." (Tātad skaitlis dalās ar 3).
+    elif skaitlis % 3 == 0:  # Dalot skaitli ar 3, atlikums ir nulle." (Tātad skaitlis dalās ar 3).
         print("Fizz")
-        
-    elif skaitlis % 5 == 0:  #Dalot skaitli ar 5, atlikums ir nulle." (Tātad skaitlis dalās ar 5)
+    elif skaitlis % 5 == 0:  # Dalot skaitli ar 5, atlikums ir nulle." (Tātad skaitlis dalās ar 5)
         print("Buzz")
-        
+    elif skaitlis % 7 == 0:  # Dalot skaitli ar 7, atlikums ir nulle." (Tātad skaitlis dalās ar 7)
+        print("Jazz")
     else:
         print(skaitlis)
